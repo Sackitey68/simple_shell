ce3ea5e45645afd334524124eca62b2aeff0690a
root <sackitey68@gmail.com>
2023-05-13 03:14:44 -0700

    Task 0

---
+++ bettycheck.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Starting point of C program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("A beautiful simple code that passes the Betty checks\n");
+return (0);
+}
